25cdc697ca72b9cc3b574bbfd7a4d1f03236b513
johnatt3 <65038575+johnatt3@users.noreply.github.com>
2021-10-03 21:58:33 -0700

    rorrim

--- src/mock/data.js
+++ src/mock/data.js
@@ -33,7 +33,7 @@ export const aboutData = {
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
+    img: 'rorrim.jpg',
     title: 'RORRIM: Health-Centric Smart Mirror',
     info: 'For our senior design project, our team designed, developed, and constructed a health-centric smart mirror that incorporates smart devices and machine learning \
     to promote a healthy lifestyle.',
